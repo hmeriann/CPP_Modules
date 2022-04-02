8c9a6df458498e9ec2bf3d6e136146f5426b6337
Heidi Merianne <hmeriann@ev-f4.kzn.21-school.ru>
2022-04-02 21:53:42 +0300

    cosmetic

---
+++ M01/ex01/Zombie.hpp
@@ -0,0 +1,32 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+private:
+	std::string	_name;
+public:
+	Zombie(std::string name);
+	~Zombie();
+
+	std::string	GetName( void ) const;
+	void		SetName(std::string name);
+
+	void	announce( void );
+};
+
+Zombie*	zombieHorde( int N, std::string name );
+
+#define RESET	"\033[0m"
+#define BLACK	"\033[30m"		/* Black */
+#define RED		"\033[31m"		/* Red */
+#define GREEN	"\033[32m"		/* Green */
+#define YELLOW	"\033[33m"		/* Yellow */
+#define BLUE	"\033[34m"		/* Blue */
+#define MAGENTA	"\033[35m"		/* Magenta */
+#define CYAN	"\033[36m"		/* Cyan */
+#define WHITE	"\033[37m"		/* White */
+#endif
